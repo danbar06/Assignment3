29335831a992b33129794055cc00e1658e86a6fd
danbar06 <44751586+danbar06@users.noreply.github.com>
2018-12-28 15:15:52 +0200

    Update BGSProtocol.java

--- Server/src/main/java/bgu/spl/net/api/bidi/BGSProtocol.java
+++ Server/src/main/java/bgu/spl/net/api/bidi/BGSProtocol.java
@@ -1,4 +1,3 @@
-
 package bgu.spl.net.api.bidi;
 
 import java.util.HashSet;
@@ -6,8 +5,8 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
-import java.util.Vector;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class BGSProtocol implements BidiMessagingProtocol<String> {
 	private volatile static Set<String> connected;
@@ -26,6 +25,7 @@ public class BGSProtocol implements BidiMessagingProtocol<String> {
 		this.id=connectionId;
 		this.usersInfo = UsersInfo.getInstance();
 		shouldTerminate=false;
+		currentUser = "";
 		synchronized(lock) {
 			if(connected==null) connected = new HashSet<>();
 			if(savedMsg==null) savedMsg = new LinkedList<>();
@@ -34,61 +34,65 @@ public class BGSProtocol implements BidiMessagingProtocol<String> {
 
 	@Override
 	public void process(String message) {
-		String code = message.substring(0, message.indexOf(" "));
+		int code = Integer.parseInt(message.substring(0, message.indexOf(" ")));
 		String command = message.substring(message.indexOf(" ")+1);
 		Map<String,String> registered = usersInfo.registered;
 		Map<String,Integer> userToId = usersInfo.userToId;
 		Map<String,Set<String>> whoFollowsMe = usersInfo.whoFollowsMe;
 		Map<String,Queue<String>> pending = usersInfo.pending;
-		Map<String,int[]> stat = usersInfo.stat;
+		Map<String, AtomicInteger[]> stat = usersInfo.stat;
 		String username;
 		String pass;
 		switch(code) {
-			case "REGISTER":
+			case 1:
 				username = command.substring(0, command.indexOf(" "));
 				pass = command.substring(command.indexOf(" ")+1, command.length()-1);
 				if(registered.containsKey(username)) {
 					connections.send(id,"ERROR 1");
 				}
 				else {
-					connections.send(id,"ACK 1");
 					registered.put(username, pass);
 					userToId.put(username, id);
 					whoFollowsMe.put(username, new HashSet<>());
 					pending.put(username, new ConcurrentLinkedQueue<>());
-					stat.put(username, new int[3]);
+					stat.put(username, new AtomicInteger[3]);
+					connections.send(id,"ACK 1");
 				}
 				break;
-			case "LOGIN":
+			case 2:
 				username = command.substring(0, command.indexOf(" "));
 				pass = command.substring(command.indexOf(" ")+1, command.length()-1);
 				if(!registered.containsKey(username) || !registered.get(username).equals(pass))
 					connections.send(id,"ERROR 2");
-				else synchronized(connected) {
-					if(connected.contains(username))
+				else {
+					if(currentUser.equals(""))
 						connections.send(id,"ERROR 2");
 					else {
-						connected.add(username);
-						currentUser=username;
-						userToId.put(currentUser, id);
-						connections.send(id,"ACK 2");
-						for(String s: pending.get(currentUser))
-							connections.send(id, s);
+						synchronized(connected) {
+							currentUser=username;
+							connected.add(currentUser);
+							userToId.put(currentUser, id);
+							connections.send(id,"ACK 2");
+							for(String s: pending.get(currentUser))
+								connections.send(id, s);
+						}
 					}
 				}
 				break;
-			case "LOGOUT":
-				if(!connected.contains(currentUser))
+			case 3:
+				if(currentUser.equals(""))
 					connections.send(id,"ERROR 3");
 				else {
-					connected.remove(currentUser);
-					userToId.remove(currentUser,id);
-					connections.send(id,"ACK 3");
-					shouldTerminate=true;
+					synchronized(connected) {
+						connected.remove(currentUser);
+						userToId.remove(currentUser,id);
+						connections.send(id,"ACK 3");
+						shouldTerminate=true;
+					}
 				}
 				break;
-			case "FOLLOW":
-				if(connected.contains(currentUser)) {
+			case 4:
+				if(!currentUser.equals("")) {
 					int follow = Integer.parseInt(command.substring(0, command.indexOf(" ")));
 					command=command.substring(command.indexOf(" ")+1);
 					int numOfUsers = Integer.parseInt(command.substring(0, command.indexOf(" ")));
@@ -99,32 +103,36 @@ public class BGSProtocol implements BidiMessagingProtocol<String> {
 						for(int i=0;i<numOfUsers;i++) {
 							int index=command.indexOf(" ");
 							username=command.substring(0, index);
-							if(canFollow(username)) {
-								whoFollowsMe.get(username).add(currentUser);
-								stat.get(username)[1]++;
-								succeed +=username;
-								countSucceed++;
+							synchronized(whoFollowsMe) {
+								if(canFollow(username)) {
+									whoFollowsMe.get(username).add(currentUser);
+									stat.get(username)[1].incrementAndGet();
+									stat.get(currentUser)[2].incrementAndGet();
+									succeed +=username+'\0';
+									countSucceed++;
+								}
 							}
 							command=command.substring(index+1);
 						}
-						stat.get(currentUser)[2] +=countSucceed;
 					}
 					else {
 						for(int i=0;i<numOfUsers;i++) {
 							int index=command.indexOf(" ");
 							username=command.substring(0, index);
-							if(canUnfollow(username)) {
-								whoFollowsMe.get(username).remove(currentUser);
-								stat.get(username)[1]--;
-								succeed +=username;
-								countSucceed++;
+							synchronized(whoFollowsMe) {
+								if(canUnfollow(username)) {
+									whoFollowsMe.get(username).remove(currentUser);
+									stat.get(username)[1].decrementAndGet();
+									stat.get(currentUser)[2].decrementAndGet();
+									succeed +=username+'\0';
+									countSucceed++;
+								}
 							}
 							command=command.substring(index+1);
 						}
-						stat.get(currentUser)[2] -=countSucceed;
 					}
 					if(countSucceed!=0)
-						connections.send(id,"ACK 4 " + follow +" "+countSucceed+" "+succeed+'\0');
+						connections.send(id,"ACK 4 " + follow +" "+countSucceed+" "+succeed);
 					else
 						connections.send(id,"ERROR 4");
 				}
@@ -132,54 +140,63 @@ public class BGSProtocol implements BidiMessagingProtocol<String> {
 					connections.send(id,"ERROR 4");	
 				break;
 
-			case "POST":
-				if(!connected.contains(currentUser))
+			case 5:
+				if(currentUser.equals(""))
 					connections.send(id,"ERROR 5");
 				else {
-					stat.get(currentUser)[0]++;
+					stat.get(currentUser)[0].incrementAndGet();
+					String content =command.substring(0, command.length()-1);
+					synchronized(savedMsg) {savedMsg.add(content);}
 					Set<String> toSend = new HashSet<>(whoFollowsMe.get(currentUser));
 					toSend.addAll(Tagged(command));
-					for(String s: toSend) {
-						if(connected.contains(s))
-							connections.send(userToId.get(s),"NOTIFICATION Public "+currentUser+" "+command);
-						else
-							pending.get(s).add("NOTIFICATION Public "+currentUser+" "+command);
+					synchronized(connected) {
+						for(String s: toSend) {
+							if(connected.contains(s))
+								connections.send(userToId.get(s),"NOTIFICATION Public "+currentUser+" "+content);
+							else
+								pending.get(s).add("NOTIFICATION Public "+currentUser+" "+content);
+						}
 					}
 				}
 				break;
 
-			case "PM":
+			case 6:
 				String recipient=command.substring(0, command.indexOf(" "));
-				if(!connected.contains(currentUser) || !registered.containsKey(recipient))
+				if(currentUser.equals("") || !registered.containsKey(recipient))
 					connections.send(id,"ERROR 6");
 				else {
-					savedMsg.add(command);
+					synchronized(savedMsg) {savedMsg.add(command);}
 					String content=command.substring(command.indexOf(' ')+1, command.length()-1);
-					if(connected.contains(recipient))
-						connections.send(userToId.get(recipient),"NOTIFICATION PM "+currentUser+" "+content);
-					else
-						pending.get(recipient).add("NOTIFICATION PM "+currentUser+" "+content);
+					synchronized(connected) {
+						if(connected.contains(recipient))
+							connections.send(userToId.get(recipient),"NOTIFICATION PM "+currentUser+" "+content);
+						else
+							pending.get(recipient).add("NOTIFICATION PM "+currentUser+" "+content);
+					}
 				}
 				break;
 
-			case "USERLIST":
-				if(!connected.contains(currentUser))
+			case 7:
+				if(currentUser.equals(""))
 					connections.send(id,"ERROR 7");
 				else {
-					String ans="ACK 7 "+registered.entrySet().size();
-					for (Map.Entry<String, String> pair : registered.entrySet()) {
-						ans+=pair.getKey();
+					String ans;
+					synchronized(registered) {
+						ans="ACK 7 "+registered.entrySet().size()+" ";
+						for (Map.Entry<String, String> pair : registered.entrySet()) {
+							ans+=pair.getKey()+'\0';
+						}
 					}
 					connections.send(id, ans);
 				}
 				break;
 
-			case "STAT":
+			case 8:
 				username = command.substring(0, command.length()-1);
-				if(!connected.contains(currentUser) || !registered.containsKey(username))
+				if(currentUser.equals("") || !registered.containsKey(username))
 					connections.send(id,"ERROR 8");
 				else {
-					String ans = stat.get(username)[0]+" "+stat.get(username)[1]+" "+stat.get(username)[2];
+					String ans = stat.get(username)[0].get()+" "+stat.get(username)[1].get()+" "+stat.get(username)[2].get();
 					connections.send(id,"ACK 8 "+ans);
 				}	
 				break;
@@ -205,13 +222,15 @@ public class BGSProtocol implements BidiMessagingProtocol<String> {
 
 	private boolean canFollow(String s) {
 		Map<String,Set<String>> whoFollowsMe = usersInfo.whoFollowsMe;
-		if(whoFollowsMe.get(s).contains(currentUser))
+		Map<String,String> registered = usersInfo.registered;
+		if(whoFollowsMe.get(s).contains(currentUser) || !registered.containsKey(s))
 			return false;
 		return true;
 	}
 	private boolean canUnfollow(String s) {
 		Map<String,Set<String>> whoFollowsMe = usersInfo.whoFollowsMe;
-		if(!whoFollowsMe.get(s).contains(currentUser))
+		Map<String,String> registered = usersInfo.registered;
+		if(!registered.containsKey(s) || !whoFollowsMe.get(s).contains(currentUser))
 			return false;
 		return true;
 	}
